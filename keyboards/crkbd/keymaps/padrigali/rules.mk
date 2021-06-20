bbd5feddd1a27bff08933d5756df9b6011b04ba6
Paddy Darley <p.l.darley@gmail.com>
2021-06-20 18:44:40 +0100

    More formatting and a snapshot of the state after some time with changes

--- keyboards/crkbd/keymaps/padrigali/rules.mk
+++ keyboards/crkbd/keymaps/padrigali/rules.mk
@@ -1,10 +1,14 @@
 RGBLIGHT_ENABLED = yes
+EXTRAKEY_ENABLED = no
+NKRO_ENABLE      = yes 
+BOOTLOADER	 = caterina
+
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
         ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+        ./lib/mode_icon_reader.c \
+        ./lib/host_led_state_reader.c \
+        ./lib/timelogger.c \
